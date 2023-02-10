97d6228382548953ec8379b567eaec4ac02e33f2
M.Henni <48835175+Mohenni@users.noreply.github.com>
2023-02-10 17:21:29 +0100

    Update QI_App.R

--- QI_App.R
+++ QI_App.R
@@ -1,75 +1,148 @@
 library(shiny)
+library(ggplot2)
 
 ui <- shinyUI(fluidPage(
-  
-  headerPanel("QI Helper"),
+  headerPanel("Quantised Inertia Helper"),
   
   # Hide errors
-  tags$style(type="text/css",".shiny-output-error { visibility: hidden; }",".shiny-output-error:before { visibility: hidden; }"),
+  tags$style(
+    type = "text/css",
+    ".shiny-output-error { visibility: hidden; }",
+    ".shiny-output-error:before { visibility: hidden; }"
+  ),
   
   tabsetPanel(
-  
     tabPanel("Thrust calculator",
              
-             fluidRow(column(4,wellPanel(
+             fluidRow(
+               column(
+                 4,
+                 wellPanel(
+                   numericInput(
+                     "V",
+                     "Current voltage (in volts)",
+                     min = 0,
+                     value = 5000
+                   ),
+                   numericInput(
+                     "d",
+                     "Distance between capacitor plates (in meters)",
+                     min = 0,
+                     value = 1 * 10 ^ (-5)
+                   ),
+                   numericInput(
+                     "A",
+                     "Surface area of the capacitor (in meters squared)",
+                     min = 0,
+                     value = 0.0016
+                   ),
+                   numericInput(
+                     "variance",
+                     "Prediction error (in %)",
+                     min = 0,
+                     max = 99.99,
+                     value = 20
+                   ),
+                   
+                   wellPanel(
+                     helpText("Graphical settings"),
+                     
+                     splitLayout(
+                       cellWidths = c("33.33%", "33.33%","33.33%"),
+                       numericInput(
+                         "V_step",
+                         "Voltage step",
+                         min = 1,
+                         max = 10000,
+                         value = 1000
+                       ),
+                       numericInput(
+                         "V_max",
+                         "Max voltage",
+                         min = 0,
+                         value = 10000
+                       ),
+                       numericInput(
+                         "V_min",
+                         "Min voltage",
+                         min = 1,
+                         value = 1
+                       )
+                       
+                     ),
+                     
+                     splitLayout(
+                       cellWidths = c("50%", "50%"),
+                       numericInput(
+                         "T_max",
+                         "Max thrust",
+                         min = 0,
+                         value = 2000
+                       ),
+                       numericInput(
+                         "T_min",
+                         "Min thrust",
+                         min = 0,
+                         value = 250
+                       )
+                       
+                     )
+                     
+                   )
+                   
+                   
+                 )
+               )
+               ,
                
-               numericInput("V",
-                            "Current voltage (in volts)", min = 0, value = 5000),
-               numericInput("d",
-                            "Distance between capacitor plates (in meters)", min = 0, value = 1 * 10^(-5)),
-               numericInput("A",
-                            "Surface area of the capacitor (in meters squared)", min = 0, value = 0.0016)
+               column(6, wellPanel(textOutput("thrust_result")),
+                      wellPanel(
+                        helpText("Plot with variation on voltage:"),
+                        plotOutput("plot_voltage_vs_thrust")
+                      ))
                
-             )
-             
-             )
-             ,
-             
-             column(6,wellPanel(
-               textOutput("thrust_result")),
-               wellPanel(
-                 helpText("Plot with variation on voltage:"),
-                 plotOutput("plot_voltage_vs_thrust"))
-             )
-             
-             )      
-             
-             ),
-  
+             )),
+    
     tabPanel("Plate distance calculator",
              
-             fluidRow(column(4,wellPanel(
+             fluidRow(
+               column(4, wellPanel(
+                 numericInput(
+                   "Weight",
+                   "Total weight of the payload + capacitor (in grams)",
+                   min = 0,
+                   value = 250
+                 ),
+                 numericInput(
+                   "SurfaceArea",
+                   "Surface area of the capacitor (in meters squared)",
+                   min = 0,
+                   value = 0.0016
+                 ),
+                 numericInput(
+                   "Current",
+                   "Used electrical current in (in Amperes)",
+                   min = 0,
+                   value = 0.0001
+                 )
+                 
+               ))
+               ,
                
-               numericInput("Weight",
-                            "Total weight of the payload + capacitor (in grams)", min = 0, value = 250),
-               numericInput("SurfaceArea",
-                            "Surface area of the capacitor (in meters squared)", min = 0, value = 0.0016),
-               numericInput("Current",
-                            "Used electrical current in (in Amperes)", min = 0, value = 0.0001)
+               column(6, wellPanel(textOutput("distance_result")),
+                      wellPanel(
+                        helpText(
+                          "Plot with variation on the capacitor plates distance:"
+                        ),
+                        plotOutput("plot_distance_vs_weightloss")
+                      ))
                
-             )
-             
-             )
-             ,
-             
-             column(6,wellPanel(
-               textOutput("distance_result")),
-               wellPanel(
-                 helpText("Plot with variation on the capacitor plates distance. The red line represents the minimal distance for the lift:"),
-                 plotOutput("plot_distance_vs_weightloss")
-               )
-             )
-             
-             )
-             
-             )
-
-  
-                      
- 
-  ),hr(),
-  print("Created & designed by M.Henni (2023)"))
-)
+             ))
+    
+  ),
+  hr(),
+  print("Created & designed by M.Henni (2023)")
+))
 
 server <- function(input, output, session) {
 
@@ -89,15 +162,15 @@ server <- function(input, output, session) {
         
         d_variation[d_variation < 0] <- NA
         
-        Weight_variation <- input$Weight - (((0.00014 * input$Current * input$SurfaceArea)/(d_variation^2))/9.80665)*100
+        Weight_variation <- input$Weight - (((0.00014 * input$Current * input$SurfaceArea)/(d_variation^2))/9.80665)*100 
         
-        plot(x = d_variation , y = Weight_variation, ylab = "Weight change (g)", xlab = "Distance between the capacitor plates (m)", type = "b", pch = 16)
-        abline(v = capacitor_plates_distance, col = "red", lwd = 1)
+        plot(x = d_variation*pi , y = Weight_variation, ylab = "Weight change (g)", xlab = "Distance between the capacitor plates (m)", type = "b", pch = 16)
+        abline(h = 0,v = capacitor_plates_distance*pi, col = "red", lwd = 1)
         
       })
       
       
-      paste("The theoretical minimal distance required between the plates of the capacitor for a positive lift is: ",round(capacitor_plates_distance,9), " meters")
+      paste("The theoretical minimal distance required between the plates of the capacitor for a positive lift is: ",round(capacitor_plates_distance*pi,9), " meters.")
       
       })
     
@@ -116,25 +189,38 @@ server <- function(input, output, session) {
       
       QI_FORCE <- (0.00014 * I * input$A) / (input$d^2)
       
-      
       P = input$V * I # in W
       
       Thrust = (QI_FORCE / P) * 1000 # in N/kW
       
+      Variance <- 1 - input$variance/100
+      
       output$plot_voltage_vs_thrust <- renderPlot({
         
-        V_variation <- c(input$V-5000,input$V-4000,input$V-3000,input$V-2000,input$V-1000,input$V,input$V+1000,input$V+2000,input$V+3000,input$V+4000,input$V+5000)
+        V_variation <- c(input$V - seq(5000,input$V_min,-input$V_step), input$V + seq(0,5000,input$V_step))
         
-        V_variation[V_variation < 0] <- NA
+        V_variation[V_variation <= 0] <- NA
+        
+        V_variation[V_variation < input$V_min] <- NA
+        
+        V_variation[V_variation > input$V_max] <- NA
         
         Thrust_variation <- QI_FORCE / (I * V_variation) * 1000
         
-        plot(x = V_variation , y = Thrust_variation, ylab = "Thrust (N / kW)", xlab = "Voltage (V)", type = "b", pch = 16)
+        Data <- data.frame(X = V_variation,Y = Thrust_variation, Y_min=Thrust_variation*(Variance), Y_max = Thrust_variation*(1/Variance))
+        
+        Data <- na.omit(Data)
+        
+        ggplot(Data, aes(x=X, y=Y)) + geom_line(aes(y = Y_max), colour = "red",lwd=1.5) +
+          geom_line(aes(y = Y_min), colour = "red",lwd=1.5) + geom_point(size=3, shape=16) + geom_line() +
+          ylab("Thrust (N / kW)") + xlab("Voltage (V)") + coord_cartesian(ylim = c(input$T_min,input$T_max)) + theme_minimal(base_size = 18)
         
       })
       
       
-      paste("The predicted thrust of the capacitor is: ", round(Thrust,3), " N / kW")
+      paste("The predicted thrust of the capacitor is: ", round(Thrust,3), " N / kW.",
+            " Considering an estimation error of ",input$variance,"%, the actual thrust should fall within the following interval: ",
+            "[",round(Thrust*(Variance),3),";",round(Thrust*(1/Variance),3),"]"," N / kW.")
       
     })
 }
